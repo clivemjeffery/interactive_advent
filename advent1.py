ecb536f5b15a138f82036daa743ece2deea0c658
clivemjeffery <clive@rosedene.com>
2016-12-15 23:03:10 +0000

    Working to motes

--- advent1.py
+++ advent1.py
@@ -11,12 +11,23 @@ mote.configure_channel(3, 16, False)
 mote.configure_channel(4, 16, False)
 
 # A dictionary of days mapped to a list of mote numbers (first is the strip, rest are motes)
-day_motes = {1:[3,1],2:[3,15],19:[3,9,10,11]}
+day_motes = {
+    1:[3,1],
+    2:[3,15],
+    3:[3,7],
+    9:[3,4,5],
+    10:[3,13],
+    19:[3,9,10,11],
+    20:[1,7],
+    21:[4,1],
+    24:[1,12,13,14,15,16]
+}
 
 @click.command()
 @click.option('--day', default=datetime.today().day, help='The December day to show advent for.')
 
 def run_advent(day):
+    mote.clear()
     click.echo('Running advent for December {}.'.format(day))
     for iday in range(1, day+1):
     	if (iday in day_motes):
@@ -25,10 +36,10 @@ def run_advent(day):
     		for imote in range(1, len(mote_list)):
     			strip = mote_list[0]
     			click.echo('calling mote.setpixel({},{},red,green,blue)'.format(strip, mote_list[imote]))
-    			mote.set_pixel(strip, mote_list[imote], 100, 100, 100)
+    			mote.set_pixel(strip, mote_list[imote] - 1, 100, 100, 100)
     	else:
     		click.echo('{} : motes not yet defined.'.format(iday))
-
+        mote.show()
     	time.sleep(0.5)
 
 
